f097541c65195cebdfae1a086490e6017c4f1051
Dila2000 <dilshandissanayake769@gmail.com>
2026-06-11 01:10:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
 function showMessage(){
-    alert("Welcome to my professional website!");
+    alert("Email: dilshandissanayake769@gmail.com");
 }
+
+function toggleTheme(){
+    document.body.classList.toggle("dark-mode");
+}
+
+let text = "Dilshan Dissanayake";
+let i = 0;
+
+function typing(){
+    if(i < text.length){
+        document.getElementById("title").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing,100);
+    }
+}
+
+typing();
+
+let count = localStorage.getItem("visits");
+
+if(count == null){
+    count = 1;
+}else{
+    count++;
+}
+
+localStorage.setItem("visits", count);
+
+document.getElementById("visits").innerHTML =
+"Visits: " + count;
